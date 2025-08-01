680573bf755e8455dd5dda8627b77262e013250c
JohnMV <johnmarulanda74@gmail.com>
2025-08-01 11:04:46 -0500

    feat(tailwind): add primary and secondary color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          100: '#EEE3FF',
+          600: '#8054C7',
+          700: '#5A3696',
+        },
+        secondary: {
+          600: '#63D838',
+        },
+      },
+    },
   },
   plugins: [],
 }
-
