37c63c54cecbd7186eed2243f6f6903a809313d4
JohnMV <johnmarulanda74@gmail.com>
2025-07-31 19:24:30 -0500

    feat(graphql): add characters query for fetching character data

---
+++ src/graphql/queris.ts
@@ -0,0 +1,16 @@
+import { gql } from '@apollo/client';
+
+export const GET_CHARACTERS = gql`
+  query GetCharacters {
+    characters {
+      results {
+        id
+        image
+        name
+        status
+        species
+        gender
+      }
+    }
+  }
+`;
